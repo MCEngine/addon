7ec1c1ae2c71330196e0e83a3c32d377348d42fe
JetsadaWijit <jetsadawijit@outlook.com>
2025-06-11 02:54:27 +0000

    chore: Set up complete codebase

---
+++ server/papermc/engine/src/main/java/io/github/mcengine/papermc/addon/engine/MCEngineAddOnPaperMC.java
@@ -0,0 +1,32 @@
+package io.github.mcengine.papermc.addon.engine;
+
+import io.github.mcengine.api.mcengine.MCEngineApi;
+import io.github.mcengine.api.mcengine.Metrics;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MCEngineAddOnPaperMC extends JavaPlugin {
+
+    /**
+     * Called when the plugin is enabled.
+     */
+    @Override
+    public void onEnable() {
+        new Metrics(this, 25751);
+        saveDefaultConfig(); // Save config.yml if it doesn't exist
+
+        boolean enabled = getConfig().getBoolean("enable", false);
+        if (!enabled) {
+            getLogger().warning("Plugin is disabled in config.yml (enable: false). Disabling plugin...");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        MCEngineApi.checkUpdate(this, getLogger(), "github", "MCEngine", "addon-engine", getConfig().getString("github.token", "null"));
+    }
+
+    /**
+     * Called when the plugin is disabled.
+     */
+    @Override
+    public void onDisable() {}
+}
