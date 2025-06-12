430f0401cff66b23934de83a6acd9d0713c77485
JetsadaWijit <jetsadawijit@outlook.com>
2025-06-12 15:15:29 +0700

    chore: Delete two unused classes

--- server/spigotmc/engine/src/main/java/io/github/mcengine/spigotmc/addon/engine/MCEngineAddOnSpigotMC.java
+++ server/spigotmc/engine/src/main/java/io/github/mcengine/spigotmc/addon/engine/MCEngineAddOnSpigotMC.java
@@ -2,8 +2,6 @@ package io.github.mcengine.spigotmc.addon.engine;
 
 import io.github.mcengine.api.mcengine.MCEngineApi;
 import io.github.mcengine.api.mcengine.Metrics;
-import io.github.mcengine.common.addon.command.AddonCommand;
-import io.github.mcengine.common.addon.tabcompleter.AddOnTabCompleter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MCEngineAddOnSpigotMC extends JavaPlugin {
@@ -22,9 +20,6 @@ public class MCEngineAddOnSpigotMC extends JavaPlugin {
             getServer().getPluginManager().disablePlugin(this);
             return;
         }
-
-        getCommand("addon").setExecutor(new AddonCommand());
-        getCommand("addon").setTabCompleter(new AddOnTabCompleter());
 
         MCEngineApi.checkUpdate(this, getLogger(), "github", "MCEngine", "addon-engine", getConfig().getString("github.token", "null"));
     }
